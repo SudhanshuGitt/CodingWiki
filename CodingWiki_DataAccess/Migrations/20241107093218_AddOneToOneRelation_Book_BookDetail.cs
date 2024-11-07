c9ae2efe19a92c779afdc898e9e887df4bdec26e
Sudhanshu Sharma <sudhanshus9530@gmail.com>
2024-11-07 15:05:11 +0530

    One to One Reationship EF

---
+++ CodingWiki_DataAccess/Migrations/20241107093218_AddOneToOneRelation_Book_BookDetail.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOneToOneRelation_Book_BookDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBM",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Book_Id",
+                table: "BookDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookDetails_Book_Id",
+                table: "BookDetails",
+                column: "Book_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookDetails_Books_Book_Id",
+                table: "BookDetails",
+                column: "Book_Id",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookDetails_Books_Book_Id",
+                table: "BookDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookDetails_Book_Id",
+                table: "BookDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Book_Id",
+                table: "BookDetails");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBM",
+                table: "Books",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
