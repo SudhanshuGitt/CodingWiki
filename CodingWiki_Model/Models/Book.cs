15c3fb4bdcf2f36ee6a929ba7d354c3db0d13f53
Sudhanshu Sharma <sudhanshus9530@gmail.com>
2024-11-07 14:50:21 +0530

    One to One Realtionship

--- CodingWiki_Model/Models/Book.cs
+++ CodingWiki_Model/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,16 @@ namespace CodingWiki_Model.Models
         // it will tell EF this column will be primary key
         // if we rename to id EF core will assume it as PK we don't need to add KEY annotation
         // or if property ends with Id eg BookId it will also Automatically be PK
+        //[Key]
         public int BookId { get; set; }
+
         public string Title { get; set; }
+        [MaxLength(20)]
+        [Required]
         public string ISBM { get; set; }
         public decimal Price { get; set; }
+        [NotMapped] // we do not want to create column for this
+        public string PriceRange { get; set; }
+        public BookDetail BookDetail { get; set; }
     }
 }
